f2a7b7147bc2ab69d4bb45e38461ae48be4eae42
Rodrigo <rodrigo.noschang1@gmail.com>
2023-02-14 15:56:59 -0300

    Done with tab bar navigation styling of the icons, can move on to the Home Page implementation now

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,10 +1,9 @@
 import { createBottomTabNavigator, BottomTabNavigationProp } from "@react-navigation/bottom-tabs";
+import { Icon, useTheme } from "native-base";
+import { Octicons } from '@expo/vector-icons';
 
 import Home from "@screens/Home";
-import SignIn from "@screens/SignIn";
 import SignOut from "@screens/SignOut";
-
-import AuthRoutes from "./auth.routes";
 
 type AppRoutes = {
     home: undefined,
@@ -17,22 +16,61 @@ export type AppRoutesNavigatorProps = BottomTabNavigationProp<AppRoutes>
 const { Navigator, Screen } = createBottomTabNavigator<AppRoutes>();
 
 const AppRoutes = () => {
+    const { colors } = useTheme();
 
     return (
-        <Navigator screenOptions = {{ headerShown: false }} initialRouteName = "home">
+        <Navigator 
+            screenOptions = {{ 
+                headerShown: false, 
+                tabBarShowLabel: false,
+                tabBarInactiveTintColor: colors.gray[500],
+                tabBarActiveTintColor: colors.gray[300]
+            }} 
+            initialRouteName = "home"
+        >
             <Screen 
                 name = 'home'
                 component = {Home}
+                options={{
+                    tabBarIcon: ({ color }) => (
+                        <Icon 
+                            as = {Octicons}
+                            name = 'home'
+                            size = {6}
+                            color = {color}
+                        />
+                    )
+                }}
             />
 
             <Screen 
                 name = 'myAdds'
                 component = {Home}
+                options = {{
+                    tabBarIcon: ({ color }) => (
+                        <Icon 
+                            as = {Octicons}
+                            name = 'tag'
+                            size = {6}
+                            color = {color}
+                        />
+                    )
+                }}
             />
 
             <Screen 
                 name = 'signOut'
                 component = {SignOut}
+                options={{
+                    tabBarIcon: () => (
+                        <Icon 
+                            as = {Octicons}
+                            name = 'sign-out'
+                            size = {6}
+                            color = 'red.100'
+                        />
+                    ),
+                }}
             />
 
         </Navigator>
