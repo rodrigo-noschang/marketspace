8769157e841a8ed29f43ebfbcd36c45a087b5b12
Rodrigo <rodrigo.noschang1@gmail.com>
2023-02-17 08:31:04 -0300

    Fixed the image problem, now still have to work on the badge part of the product thumb and whatever is going to show when the user has no adds of his own

--- src/components/AddProduct.tsx
+++ src/components/AddProduct.tsx
@@ -1,4 +1,4 @@
-import { Pressable, IPressableProps, VStack, Image, Heading, Text, Badge } from 'native-base';
+import { Pressable, IPressableProps, VStack, Image, Heading, Text, Badge, HStack } from 'native-base';
 
 import { ProductAddDTO } from '@dtos/AddsDTO';
 import api from '@services/api';
@@ -9,16 +9,32 @@ type Props = IPressableProps & {
 
 const AddProduct = ({ product, ...rest }: Props) => {
 
-    console.log('AddProduct | Image path -> ', `${api.defaults.baseURL}/products/images/${product.product_images[0].path}`);
-
     return (
-        <Pressable w = '48%' {...rest}>
-            <Image 
-                w = '100%'
-                h = {20}
-                source = {{uri: `${api.defaults.baseURL}/products/images/${product.product_images[0].path}`}}
-                alt = 'Product image'
-            />
+        <Pressable w = '47%' {...rest} mt = {5}>
+            <VStack>
+                <Image 
+                    h = {24}
+                    source = {{uri: `${api.defaults.baseURL}/images/${product.product_images[0].path}`}}
+                    alt = 'Product image'
+                    rounded = 'md'
+                    borderWidth = {1}
+                    borderColor = 'gray.200'
+                />
+
+                <Text fontSize = 'md' fontFamily = 'body' color = 'gray.300' mt = {1}>
+                    {product.name}
+                </Text>
+
+                <HStack alignItems = 'flex-end'>
+                    <Heading fontFamily = 'heading' fontSize = 'sm' color = 'gray.300'>
+                        R$   
+                    </Heading>
+
+                    <Heading fontFamily = 'heading' fontSize = 'lg' color = 'gray.300' ml = {1}>
+                        {(product.price / 100).toFixed(2).replace('.', ',')}
+                    </Heading>
+                </HStack>
+            </VStack>
 
         </Pressable>
     )
--- src/screens/UserAdds.tsx
+++ src/screens/UserAdds.tsx
@@ -1,30 +1,27 @@
-import { VStack, Text, FlatList } from "native-base";
+import { VStack, Text, Heading, FlatList, HStack } from "native-base";
 
 import AddProduct from "@components/AddProduct";
 
 import { useUserAdds } from "@contexts/userAddsContext";
+import { useEffect, useState } from "react";
+import { ProductAddDTO } from "@dtos/AddsDTO";
 
 const UserAdds = () => {
     const { userAdds, fetchAndStoreUserAdds } = useUserAdds();
 
-    console.log('On UserAdds -> ', userAdds);
-
-    // useEffect(() => {
-    //     removeAdds();
-
-    // }, [])
-
     return (
-        <VStack pt = {45} px = {6}>
+        <VStack pt = {45} px = {6} flex = {1}>
             {/* TODO: IMPLEMENT THE HEADER COMPONENT THAT MIGHT HAVE A RETURN FUNCIONALITY */}
 
-            <FlatList 
-                data = {userAdds}
-                keyExtractor = {add => add.id}
-                renderItem = {({ item }) => (
-                    <AddProduct product = {item}/>
-                )}
-            />
+            <HStack flexWrap = 'wrap' justifyContent = 'space-between'>
+                {userAdds ? 
+                    userAdds.map((add, index) => (
+                        <AddProduct product = {add} key = {`${add.id}-${index}`}/>
+                    ))
+                :
+                    <Heading> Você ainda não possui anúncios </Heading>
+                }
+            </HStack>
         </VStack>
     )
 }
