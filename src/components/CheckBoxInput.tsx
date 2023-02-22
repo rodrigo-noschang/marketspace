9ac5b1ce874da968891d7ecfff1877f658c07d44
Rodrigo <rodrigo.noschang1@gmail.com>
2023-02-22 15:15:54 -0300

    Done with the add creating form, still have to do the requesting part of it, though. Gonna do that next, do not worry

---
+++ src/components/CheckBoxInput.tsx
@@ -0,0 +1,63 @@
+import { useState } from "react";
+import { Box, HStack, Text, Icon, Pressable, IPressableProps, Center } from "native-base";
+import { Feather } from '@expo/vector-icons';
+
+type Props = IPressableProps & {
+    value: string,
+    label: string,
+    options: string[],
+    updateOptions: (value: string[]) => void
+}
+
+const CheckBoxInput = ({ value, label, options, updateOptions, ...rest }: Props) => {
+    const [isSelected, setIsSelected] = useState(false);
+
+    const updatePaymentOptions = () => {
+        if (!isSelected) {
+            updateOptions([...options, value]);
+        } else {
+            const removedOption = options.filter(option => option !== value);
+            updateOptions(removedOption);
+        }
+    }
+
+    const changeBoxSelectedState = () => {
+        setIsSelected(!isSelected)
+    }
+
+    const handleChangePaymentOptions = () => {
+        updatePaymentOptions();
+        changeBoxSelectedState();
+    }
+
+    return (
+        <Pressable alignSelf = 'flex-start' onPress = {handleChangePaymentOptions} {...rest}> 
+            <HStack alignItems = 'center'>
+                <Box w = {5} 
+                    h = {5} 
+                    rounded = 'sm'
+                    borderWidth = {isSelected ? 0 : 2} 
+                    borderColor = 'gray.200' 
+                    bgColor = {isSelected ? 'blue.200' : 'gray.600'}
+                > 
+                    { isSelected &&
+                        <Center flex = {1} >
+                            <Icon 
+                                as = {Feather}
+                                size = {3}
+                                name = 'check'
+                                color = 'gray.700'
+                            />
+                        </Center>
+                    }
+                </Box>
+
+                <Text fontSize = 'lg' fontFamily = 'body' color = 'gray.200' ml = {3} > 
+                    {label} 
+                </Text>
+            </HStack>
+        </Pressable>
+    )
+}
+
+export default CheckBoxInput;
--- src/screens/NewAdd.tsx
+++ src/screens/NewAdd.tsx
@@ -1,10 +1,14 @@
 import { useState } from 'react';
 import { Switch } from 'react-native';
-import { VStack, Heading, ScrollView, Text, Radio, HStack, Box, Checkbox } from 'native-base';
+import { VStack, Heading, ScrollView, Text, Radio, HStack, Box } from 'native-base';
+import { useNavigation } from '@react-navigation/native'
+import { useAuth } from '@contexts/authContext';
 
-import AppHeader from '@components/AppHeader';
-import NewAddPhotoSelector from '@components/NewAddPhotoSelector';
 import Input from '@components/Input';
+import AppHeader from '@components/AppHeader';
+import CheckBoxInput from '@components/CheckBoxInput';
+import NewAddPhotoSelector from '@components/NewAddPhotoSelector';
+import Button from '@components/Button';
 
 type PhotoObject = {
     name: string, 
@@ -14,123 +18,181 @@ type PhotoObject = {
 
 const NewAdd = () => {
     const [productsPhotos, setProductsPhotos] = useState<PhotoObject[]>([]);
+    const [addTitle, setAddTitle] = useState('');
+    const [addDescription, setAddDescription] = useState('');
     const [productIsNew, setProductIsNew] = useState('');
+    const [productPrice, setProductPrice] = useState('');
     const [acceptsTrade, setAcceptsTrade] = useState(false);
-    const [acceptedPaymentMethods, setAcceptedPaymentMethods] = useState<string[]>([]);
+    const [selectedPaymentOptions, setSelectedPaymentOptions] = useState<string[]>([]);
 
-    console.log(acceptedPaymentMethods);
+    const navigator = useNavigation();
+    const { user } = useAuth();
 
+    const handleCreateNewAdd = () => {
+        const add = {
+            name: addTitle,
+            description: addDescription,
+            is_new: productIsNew === 'new' ? true : productIsNew === 'old' ? false : null,
+            price: productPrice,
+            user_id: user.id,
+            is_active: true
+        }
+    }
+    
     return (
-        <ScrollView pt = {45} px = {6} flex = {1} showsVerticalScrollIndicator = {false}>
-            <VStack >
+        <VStack flex = {1}>
+            <Box pt = {45} px = {6} bgColor = 'gray.600'>
                 <AppHeader 
                     title = 'Criar anúncio'
                     returnable
                 />
+            </Box>
 
-                <Heading fontSize = 'lg' color = 'gray.200' fontFamily = 'heading' mt = {2}>
-                    Imagens
-                </Heading>
+            <ScrollView showsVerticalScrollIndicator = {false} bgColor = 'gray.700' pb = {20}> 
+                <VStack px = {6} bgColor = 'gray.600' pb = {4}>
+                    <Heading fontSize = 'lg' color = 'gray.200' fontFamily = 'heading' mt = {2}>
+                        Imagens
+                    </Heading>
 
-                <Text fontSize = 'md' color = 'gray.200' fontFamily = 'body' mt = {1}>
-                    Escolha até 3 imagens para mostrar o quanto o seu produto é incrível!
-                </Text>
+                    <Text fontSize = 'md' color = 'gray.200' fontFamily = 'body' mt = {1}>
+                        Escolha até 3 imagens para mostrar o quanto o seu produto é incrível!
+                    </Text>
 
-                <NewAddPhotoSelector 
-                    productsPhotos = {productsPhotos}
-                    setProductsPhotos = {setProductsPhotos}
-                />
-
-                <Heading fontSize = 'lg' color = 'gray.200' fontFamily = 'heading' mt = {6}>
-                    Sobre o produto
-                </Heading>
-
-                <Input
-                    mt = {3} 
-                    placeholder = 'Título do anúncio'
-                />
-
-                <Input 
-                    mt = {3}
-                    placeholder = 'Descrição do produto'
-                    h = {130}
-                    textAlignVertical = 'top'
-                    multiline
-                />
-
-                <Radio.Group
-                    colorScheme = 'indigo'
-                    name = 'productIsNew'
-                    value = {productIsNew}
-                    onChange = {(chosenValue) => {
-                        setProductIsNew(chosenValue)
-                    }}
-                >
-                    <HStack w = '100%' justifyContent = 'space-between' mt = {4}>
-                        <Radio value = 'new' p = {0.5}>
-                            Produto novo
-                        </Radio>
-
-                        <Radio value = 'old' p = {0.5}>
-                            Produto usado
-                        </Radio>
-                    </HStack>
-
-                </Radio.Group>
-
-                <Heading fontSize = 'lg' color = 'gray.200' fontFamily = 'heading' mt = {6}>
-                    Venda
-                </Heading>
-
-                <Input 
-                    placeholder = 'Valor do produto'
-                    preText = 'R$'
-                    mt = {2}
-                />
-
-                <Heading fontSize = 'lg' color = 'gray.200' fontFamily = 'heading' mt = {6}>
-                    Aceita troca?
-                </Heading>
-
-                <Box alignItems = 'flex-start'>
-                    <Switch 
-                        trackColor = {{false: '#D9D8DA', true: '#D9D8DA'}}
-                        thumbColor = {acceptsTrade ? '#647AC7' : '#F7F7F8'}
-                        value = {acceptsTrade}
-                        onValueChange = {() => setAcceptsTrade(!acceptsTrade)}
+                    <NewAddPhotoSelector 
+                        productsPhotos = {productsPhotos}
+                        setProductsPhotos = {setProductsPhotos}
                     />
-                </Box>
 
-                <Heading fontSize = 'lg' color = 'gray.200' fontFamily = 'heading' mt = {6}>
-                    Meios de pagamento aceitos
-                </Heading>
+                    <Heading fontSize = 'lg' color = 'gray.200' fontFamily = 'heading' mt = {6}>
+                        Sobre o produto
+                    </Heading>
 
-{/* // https://github.com/react-native-checkbox/react-native-checkbox */}
-                <Checkbox.Group value = {acceptedPaymentMethods} onChange = {setAcceptedPaymentMethods}>
-                    <Checkbox value = 'boleto'>
-                        Boleto
-                    </Checkbox>
+                    <Input
+                        mt = {3} 
+                        placeholder = 'Título do anúncio'
+                        onChangeText = {setAddTitle}
+                        />
 
-                    <Checkbox value = 'pix'>
-                        Pix
-                    </Checkbox>
+                    <Input 
+                        mt = {3}
+                        placeholder = 'Descrição do produto'
+                        h = {130}
+                        textAlignVertical = 'top'
+                        multiline
+                        onChangeText = {setAddDescription}
+                    />
 
-                    <Checkbox value = 'cash'>
-                        Dinheiro
-                    </Checkbox>
+                    <Radio.Group
+                        colorScheme = 'indigo'
+                        name = 'productIsNew'
+                        value = {productIsNew}
+                        onChange = {(chosenValue) => {
+                            setProductIsNew(chosenValue)
+                        }}
+                    >
+                        <HStack w = '100%' justifyContent = 'space-between' mt = {4}>
+                            <Radio value = 'new' p = {0.5}>
+                                <Text fontSize = 'lg' fontFamily = 'body' color = 'gray.200'>
+                                    Produto novo
+                                </Text>
+                            </Radio>
 
-                    <Checkbox value = 'card'>
-                        Cartão de Crédito
-                    </Checkbox>
+                            <Radio value = 'old' p = {0.5}>
+                                <Text fontSize = 'lg' fontFamily = 'body' color = 'gray.200'>
+                                    Produto usado
+                                </Text>
+                            </Radio>
+                        </HStack>
 
-                    <Checkbox value = 'deposit'>
-                        Depósito Bancário
-                    </Checkbox>
+                    </Radio.Group>
 
-                </Checkbox.Group>
+                    <Heading fontSize = 'lg' color = 'gray.200' fontFamily = 'heading' mt = {6}>
+                        Venda
+                    </Heading>
 
-            </VStack>
-        </ScrollView>
+                    <Input 
+                        placeholder = 'Valor do produto'
+                        preText = 'R$'
+                        mt = {2}
+                        onChangeText = {setProductPrice}
+                    />
+
+                    <Heading fontSize = 'lg' color = 'gray.200' fontFamily = 'heading' mt = {6}>
+                        Aceita troca?
+                    </Heading>
+
+                    <Box alignItems = 'flex-start'>
+                        <Switch 
+                            trackColor = {{false: '#D9D8DA', true: '#D9D8DA'}}
+                            thumbColor = {acceptsTrade ? '#647AC7' : '#F7F7F8'}
+                            value = {acceptsTrade}
+                            onValueChange = {() => setAcceptsTrade(!acceptsTrade)}
+                        />
+                    </Box>
+
+                    <Heading fontSize = 'lg' color = 'gray.200' fontFamily = 'heading' mt = {3} mb = {3}>
+                        Meios de pagamento aceitos
+                    </Heading>
+
+                    <CheckBoxInput 
+                        value = 'boleto'
+                        label = 'Boleto'
+                        options = {selectedPaymentOptions}
+                        updateOptions = {setSelectedPaymentOptions}
+                        mb = {1}
+                        
+                    />
+
+                    <CheckBoxInput 
+                        value = 'pix'
+                        label = 'Pix'
+                        options = {selectedPaymentOptions}
+                        updateOptions = {setSelectedPaymentOptions}
+                        mb = {1}
+                    />
+
+                    <CheckBoxInput 
+                        value = 'cash'
+                        label = 'Dinheiro'
+                        options = {selectedPaymentOptions}
+                        updateOptions = {setSelectedPaymentOptions}
+                        mb = {1}
+                    />
+
+                    <CheckBoxInput 
+                        value = 'card'
+                        label = 'Cartão de Crédito'
+                        options = {selectedPaymentOptions}
+                        updateOptions = {setSelectedPaymentOptions}
+                        mb = {1}
+                    />
+
+                    <CheckBoxInput 
+                        value = 'deposit'
+                        label = 'Depósito Bancário'
+                        options = {selectedPaymentOptions}
+                        updateOptions = {setSelectedPaymentOptions}
+                        mb = {1}
+                    />
+                </VStack>
+
+                <HStack w = '100%' bgColor = 'gray.700' px = {6} py = {5} justifyContent = 'space-between'>
+                    <Button 
+                        title = 'Cancelar'
+                        buttonTheme = 'light'
+                        w = '48%'
+                        onPress = {() => navigator.goBack()}
+                    />
+                    
+                    <Button 
+                        title = 'Avançar'
+                        buttonTheme = 'dark'  
+                        w = '48%'
+                        onPress = {handleCreateNewAdd} 
+                    />
+                </HStack>
+            </ScrollView>
+        </VStack>
     )
 
 }
